dee1a2a9d52f61f5925b50380b57dddf6f442685
Connor <connorthomson53@hotmail.com>
2019-10-31 16:18:29 +0000

    rock added, minor changes to additional pages

--- src/main/java/com/nationwide/sixweekproject/controller/RockController.java
+++ src/main/java/com/nationwide/sixweekproject/controller/RockController.java
@@ -3,6 +3,7 @@ package com.nationwide.sixweekproject.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.nationwide.sixweekproject.service.RockService;
 //the part that decides interactions between the database and the front end. 
 
 @RestController
+@CrossOrigin("*")
 public class RockController {
 	@Autowired
 	private RockService rock;
@@ -37,7 +39,7 @@ public class RockController {
 	
 	@PostMapping("/rock/add")
 	public String addRock(@RequestBody RockData ref) {
-		ref.setSongnum(null);
+		//ref.setSongnum(null);
 		return rock.addRock(ref);
 	}
 	
